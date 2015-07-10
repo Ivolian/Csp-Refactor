b32e4ca3e3c79bbfaf8b70417ff3cea4ab3ab6a3
renjiajian <renjiajian@withub.net.cn>
2015-07-10 19:19:03 +0800

    LoginActivity 重构完成

--- app/src/main/java/com/unicorn/csp/activity/LoginActivity.java
+++ app/src/main/java/com/unicorn/csp/activity/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.unicorn.csp.activity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 import com.ivo.flatbutton.FlatButton;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.unicorn.csp.R;
+import com.unicorn.csp.utils.ToastUtils;
 
 import net.grandcentrix.tray.TrayAppPreferences;
 
@@ -15,11 +17,15 @@ import butterknife.OnTextChanged;
 
 public class LoginActivity extends ToolbarActivity {
 
+
     // ========================== 常量 ==========================
 
-    final String ACCOUNT = "admin";
+    final String SF_ACCOUNT = "account";
 
-    final String PASSWORD = "123456";
+    final String SF_PASSWORD = "password";
+
+    final String SF_REMEMBER_ME = "remember_me";
+
 
     // ========================== Views ==========================
 
@@ -29,8 +35,12 @@ public class LoginActivity extends ToolbarActivity {
     @Bind(R.id.et_password)
     MaterialEditText etPassword;
 
+    @Bind(R.id.cb_remember_me)
+    CheckBox cbRememberMe;
+
     @Bind(R.id.btn_login)
     FlatButton btnLogin;
+
 
     // ========================== onCreate ==========================
 
@@ -45,13 +55,11 @@ public class LoginActivity extends ToolbarActivity {
 
     private void initViews() {
 
-        final TrayAppPreferences appPreferences = new TrayAppPreferences(this);
-
-        etAccount.setText(appPreferences.getString("account","default"));
-        etPassword.setText(PASSWORD);
+        restoreLoginInfo();
     }
 
-    // ========================== onTextChanged ==========================
+
+    // ========================== 登录按钮状态 ==========================
 
     @OnTextChanged(R.id.et_account)
     public void onUsernameChange() {
@@ -67,19 +75,68 @@ public class LoginActivity extends ToolbarActivity {
 
     private void onUsernameOrPasswordChange() {
 
-        String account = etAccount.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
-        btnLogin.setEnabled(!account.equals("") && !password.equals(""));
+        btnLogin.setEnabled(!isAccountEmpty() && !isPasswordEmpty());
     }
 
-    // ========================== login ==========================
+
+    // ========================== 登录 ==========================
 
     @OnClick(R.id.btn_login)
     public void login() {
 
-        final TrayAppPreferences appPreferences = new TrayAppPreferences(this);
-        appPreferences.put("account", etAccount.getText().toString().trim());
+        // todo login
+        ToastUtils.show(this, "登录成功！");
+        storeLoginInfo();
     }
 
+
+    // ========================== 记住密码 ==========================
+
+    private void storeLoginInfo() {
+
+        TrayAppPreferences trayAppPreferences = new TrayAppPreferences(this);
+        trayAppPreferences.put(SF_ACCOUNT, getAccount());
+        trayAppPreferences.put(SF_PASSWORD, getPassword());
+        trayAppPreferences.put(SF_REMEMBER_ME, cbRememberMe.isChecked());
+    }
+
+    private void restoreLoginInfo() {
+
+        try {
+            TrayAppPreferences trayAppPreferences = new TrayAppPreferences(this);
+            String account = trayAppPreferences.getString(SF_ACCOUNT);
+            String password = trayAppPreferences.getString(SF_PASSWORD);
+            boolean rememberMe = trayAppPreferences.getBoolean(SF_REMEMBER_ME, false);
+            if (rememberMe) {
+                etAccount.setText(account);
+                etPassword.setText(password);
+            }
+        } catch (Exception e) {
+            //
+        }
+    }
+
+
+    // ========================== 基础方法 ==========================
+
+    private boolean isAccountEmpty() {
+
+        return getAccount().equals("");
+    }
+
+    private boolean isPasswordEmpty() {
+
+        return getPassword().equals("");
+    }
+
+    private String getAccount() {
+
+        return etAccount.getText().toString().trim();
+    }
+
+    private String getPassword() {
+
+        return etPassword.getText().toString().trim();
+    }
 
 }
