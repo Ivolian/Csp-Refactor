2cd6a5ba5000ebcaefb8f6da22c2055b6d4d4476
renjiajian <renjiajian@withub.net.cn>
2015-07-10 12:11:53 +0800

    添加 Malinskiy/android-material-icons

--- app/src/main/java/com/unicorn/www/csp/MainActivity.java
+++ app/src/main/java/com/unicorn/www/csp/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.malinskiy.materialicons.Iconify;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +14,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Iconify.addIcons((TextView)findViewById(R.id.d));
     }
 
     @Override
