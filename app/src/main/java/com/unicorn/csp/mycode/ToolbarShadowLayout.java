03f0c9c977846f650a21fac7954c0a18378b1537
renjiajian <renjiajian@withub.net.cn>
2015-07-10 13:40:25 +0800

    添加 shadow-layout-mc

---
+++ app/src/main/java/com/unicorn/csp/mycode/ToolbarShadowLayout.java
@@ -0,0 +1,27 @@
+package com.unicorn.csp.mycode;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.dd.ShadowLayout;
+
+/**
+ * Created by Administrator on 2015/7/10.
+ */
+public class ToolbarShadowLayout extends ShadowLayout {
+
+    public ToolbarShadowLayout(Context context) {
+        super(context);
+    }
+
+    public ToolbarShadowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ToolbarShadowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
