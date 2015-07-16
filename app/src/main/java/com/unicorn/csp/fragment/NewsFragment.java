dedb4aca7269d15ceec8d13d59eaa28f88cfae31
renjiajian <renjiajian@withub.net.cn>
2015-07-16 14:09:10 +0800

    添加了一些 Utils

--- app/src/main/java/com/unicorn/csp/fragment/NewsFragment.java
+++ app/src/main/java/com/unicorn/csp/fragment/NewsFragment.java
@@ -1,5 +1,6 @@
 package com.unicorn.csp.fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -8,13 +9,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.nispok.snackbar.Snackbar;
-import com.nispok.snackbar.SnackbarManager;
 import com.unicorn.csp.R;
 import com.unicorn.csp.fragment.base.ButterKnifeFragment;
 import com.unicorn.csp.other.greenmatter.ColorOverrider;
@@ -45,12 +43,35 @@ public class NewsFragment extends ButterKnifeFragment {
         return R.layout.fragment_news;
     }
 
-    @Bind(R.id.recyclerView)
-    RecyclerView recyclerView;
-    RendererAdapter adapter;
 
     @Bind(R.id.swipeRefreshLayout)
     SwipeRefreshLayout swipeRefreshLayout;
+
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    RendererAdapter rendererAdapter;
+
+    // ==================== pager data ====================
+
+    final Integer pagerSize = 5;
+
+    Integer currentPager;
+
+    Boolean noData;
+
+    Boolean allDataLoaded;
+
+    boolean loadingMore;
+
+
+    private void initPagerData() {
+
+        currentPager = 1;
+        noData = false;
+        allDataLoaded = false;
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -60,33 +81,44 @@ public class NewsFragment extends ButterKnifeFragment {
         return rootView;
     }
 
-    private void askForData() {
+    private void reload() {
 
-        String url = "http://192.168.1.101:3002/withub/api/v1/news?pageNo=1&pageSize=10";
-        MyVolley.getRequestQueue().add(new JsonObjectRequest(url,
+        initPagerData();
+
+
+
+//        String url = "http://192.168.1.101:3002/withub/api/v1/news?pageNo=1&pageSize=10";
+        Uri.Builder builder = Uri.parse("http://192.168.1.101:3002/withub/api/v1/news?").buildUpon();
+        builder.appendQueryParameter("pageNo", currentPager.toString());
+        builder.appendQueryParameter("pageSize", "5");
+
+
+        MyVolley.getRequestQueue().add(new JsonObjectRequest(builder.toString(),
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        swipeRefreshLayout.setRefreshing(false);
                         JSONArray contents = JSONUtils.getJSONArray(response, "content", null);
-                      List<Renderable> newsList = new ArrayList<>();
-                        for (int i=0;i!=contents.length();i++){
-                          JSONObject content = JSONUtils.getJSONObject(contents,i);
-                          String title = JSONUtils.getString(content, "title", "");
+                        List<Renderable> newsList = new ArrayList<>();
+                        for (int i = 0; i != contents.length(); i++) {
+                            JSONObject content = JSONUtils.getJSONObject(contents, i);
+                            String title = JSONUtils.getString(content, "title", "");
                             JSONObject contentData = JSONUtils.getJSONObject(content, "contentData", null);
-                            String data = JSONUtils.getString(contentData,"data","");
+                            String data = JSONUtils.getString(contentData, "data", "");
 
-                                    // todo img
-                          Date time = new Date();
+                            // todo img
+                            Date time = new Date();
                             int commentCount = 11;
 
-                            newsList.add(new News(title,time,data,commentCount));
-                      }
-        adapter.update(newsList);
+                            newsList.add(new News(title, time, data, commentCount));
+                        }
+                        rendererAdapter.update(newsList);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError volleyError) {
+                        swipeRefreshLayout.setRefreshing(false);
                         ToastUtils.show(VolleyErrorHelper.getErrorMessage(volleyError));
                     }
                 }));
@@ -94,17 +126,87 @@ public class NewsFragment extends ButterKnifeFragment {
 
     private void initViews() {
 
+
         initRecyclerView();
         initSwipeRefreshLayout();
-        askForData();
+        initPagerData();
+        reload();
     }
+
+    private void loadMore() {
+
+       loadingMore = true;
+        Uri.Builder builder = Uri.parse("http://192.168.1.101:3002/withub/api/v1/news?").buildUpon();
+        builder.appendQueryParameter("pageNo", (currentPager+1)+"");
+        builder.appendQueryParameter("pageSize", "5");
+
+        MyVolley.getRequestQueue().add(new JsonObjectRequest(builder.toString(),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        loadingMore = false;
+
+
+                        JSONArray contents = JSONUtils.getJSONArray(response, "content", null);
+                        List<Renderable> newsList = new ArrayList<>();
+                        for (int i = 0; i != contents.length(); i++) {
+                            JSONObject content = JSONUtils.getJSONObject(contents, i);
+                            String title = JSONUtils.getString(content, "title", "");
+                            JSONObject contentData = JSONUtils.getJSONObject(content, "contentData", null);
+                            String data = JSONUtils.getString(contentData, "data", "");
+
+                            // todo img
+                            Date time = new Date();
+                            int commentCount = 11;
+
+                            newsList.add(new News(title, time, data, commentCount));
+                        }
+                        for (Renderable item:newsList){
+                            rendererAdapter.add(item,rendererAdapter.getItemCount());
+
+                        }
+                            rendererAdapter.notifyDataSetChanged();
+                        currentPager++;
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                      loadingMore = false;
+                        ToastUtils.show(VolleyErrorHelper.getErrorMessage(volleyError));
+                    }
+                }));
+    }
+
 
     private void initRecyclerView() {
 
         recyclerView.setHasFixedSize(false);
-        recyclerView.setLayoutManager(getLinearLayoutManager());
-         adapter = new RendererAdapter(new ArrayList<Renderable>(), new RendererBuilder(new Factory()));
-        recyclerView.setAdapter(adapter);
+        final LinearLayoutManager linearLayoutManager = getLinearLayoutManager();
+        recyclerView.setLayoutManager(linearLayoutManager);
+        rendererAdapter = new RendererAdapter(new ArrayList<Renderable>(), new RendererBuilder(new Factory()));
+        recyclerView.setAdapter(rendererAdapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+
+                if (noData || allDataLoaded || loadingMore) {
+                    return;
+                }
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    int lastVisibleItem = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+                    int totalItemCount = linearLayoutManager.getItemCount();
+                    if (totalItemCount != 0 && totalItemCount == (lastVisibleItem + 1)) {
+                        loadMore();
+                    }
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            }
+        });
     }
 
     private void initSwipeRefreshLayout() {
@@ -113,7 +215,7 @@ public class NewsFragment extends ButterKnifeFragment {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                NewsFragment.this.onRefresh();
+                reload();
             }
         });
     }
@@ -123,36 +225,15 @@ public class NewsFragment extends ButterKnifeFragment {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 swipeRefreshLayout.setRefreshing(false);
-                showMsgAfterRefresh();
             }
         }, 2000);
     }
 
-    private List<Renderable> getItems() {
-
-        List<Renderable> items = new ArrayList<>();
-        for (int i = 0; i != 10; i++) {
-            items.add(new News("最高人民法院出台服务保障“一带一路”意见", new Date(), "asdfasf", 11));
-        }
-        return items;
-    }
 
 
-    // ========================== 下拉刷新提示信息 ==========================
 
-    @Bind(R.id.container)
-    FrameLayout container;
 
-    private void showMsgAfterRefresh() {
-
-        SnackbarManager.show(Snackbar.with(getActivity())
-                .position(Snackbar.SnackbarPosition.TOP)
-                .color(0xff4caf50)
-                .margin(25, 25)
-                .text("有10条资讯热点更新！"), container);
-    }
 
 
     // ========================== 基础方法 ==========================
@@ -164,5 +245,18 @@ public class NewsFragment extends ButterKnifeFragment {
         return linearLayoutManager;
     }
 
+    // ========================== 下拉刷新提示信息 ==========================
+
+//    @Bind(R.id.container)
+//    FrameLayout container;
+//
+//    private void showMsgAfterRefresh() {
+//
+//        SnackbarManager.show(Snackbar.with(getActivity())
+//                .position(Snackbar.SnackbarPosition.TOP)
+//                .color(0xff4caf50)
+//                .margin(25, 25)
+//                .text("有10条资讯热点更新！"), container);
+//    }
 
 }
