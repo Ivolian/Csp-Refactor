eb497b8089ace06180c6009718f273ddebff02f3
renjiajian <renjiajian@withub.net.cn>
2015-09-21 16:59:56 +0800

    修改了很多

--- app/src/main/java/com/unicorn/csp/utils/ToastUtils.java
+++ app/src/main/java/com/unicorn/csp/utils/ToastUtils.java
@@ -7,10 +7,15 @@ import com.unicorn.csp.MyApplication;
 
 public class ToastUtils {
 
+    static Toast toast;
+
     public static void show(String msg) {
 
-        Toast toast = Toast.makeText(MyApplication.getInstance(), msg, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0);
+        if (toast == null) {
+            toast = Toast.makeText(MyApplication.getInstance(), "", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+        }
+        toast.setText(msg);
         toast.show();
     }
 
