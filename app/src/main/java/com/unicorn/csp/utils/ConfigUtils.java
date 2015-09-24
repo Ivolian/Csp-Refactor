f79c44af7e870040ab36f9fc18e896d1c9ec17c3
renjiajian <renjiajian@withub.net.cn>
2015-09-24 09:29:38 +0800

    给 textzoom 设置限制

--- app/src/main/java/com/unicorn/csp/utils/ConfigUtils.java
+++ app/src/main/java/com/unicorn/csp/utils/ConfigUtils.java
@@ -13,7 +13,7 @@ public class ConfigUtils {
     public static String getBaseUrl() {
 
         return "http://115.28.239.33:8080/withub";
-//        return "http://192.168.1.6:3000/withub";
+//        return "http://192.168.1.3:3000/withub";
 //        return "http://192.168.7.59:3000/withub";
 //        return "http://58.16.65.7:8090/withub";
     }
